822fd1ffd6f1ceb8ce282e771cf5352a3c944958
Marek Gmyrek <marek.gmyrek@gmail.com>
2022-03-25 16:43:48 +0100

    Cleanup of bash script

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BASEDIR=${SCRIPTDIR}/..
-
-VENVDIR=$(pipenv --venv)
-APPNAME="tasks:app"
+BASEDIR=$(dirname ${SCRIPTDIR})
 
 rm -fr ${BASEDIR}/{log,run,etc}
